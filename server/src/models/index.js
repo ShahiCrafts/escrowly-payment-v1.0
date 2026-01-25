37ff353b80b283d368990b2554abce5be9301ef3
Saugat Shahi <dev.shahi.apps@gmail.com>
2026-01-26 02:26:47 +0545

    feat(server): add models barrel export file

---
+++ server/src/models/index.js
@@ -0,0 +1,23 @@
+const User = require('./User');
+const Transaction = require('./Transaction');
+const RefreshToken = require('./RefreshToken');
+const AuditLog = require('./AuditLog');
+const Message = require('./Message');
+const Notification = require('./Notification');
+const SystemSetting = require('./SystemSetting');
+const Agreement = require('./Agreement');
+const SuspensionAppeal = require('./SuspensionAppeal');
+const UserDevice = require('./UserDevice');
+
+module.exports = {
+  User,
+  Transaction,
+  RefreshToken,
+  AuditLog,
+  Message,
+  Notification,
+  SystemSetting,
+  Agreement,
+  SuspensionAppeal,
+  UserDevice
+};
